3318ea0444ca429816260f9e0f3cefa5c538d712
Bahniman Borah <bahnimanborah.borah7@gmail.com>
2018-05-19 16:26:48 +0530

    Updated file

--- php/editprofile.php
+++ php/editprofile.php
@@ -7,52 +7,75 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile</title>
-    <link rel="stylesheet" href="css/editprofile.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/editprofile.css">
 </head>
 <body>
-    <?php
-    ?>
-    <!--navigation bar-->
-    <div class="topNavigation">
-        <div class="topNavButtons" style="float:right">
-            <a href="verify.php">Profile</a>
-            <a href="logout.php">Log Out</a>
+
+    <div class="container-fluid">
+        <!--navigation bar-->
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            
+            <div class="navbar-header">
+                <a class="navbar-brand" href="../home.htm"><em>Tour Cafe</em></a>
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#myNavBar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavBar">
+                <ul class="nav navbar-nav">
+                    <li><a href="verify.php">Profile</a></li>
+                    <li><a href="logout.php">Log Out</a></li>
+                </ul>
+            </div>
+
+        </nav>
+
+        <!-- body -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div id="editBox">
+                    <h2 class="text-center">Edit Profile</h2>
+                <div class="form-horizontal">
+                        <div class="form-group">
+                            <form action="editprofile.php" method="post">
+                                
+                                <select class="form-control" name="fields">
+                                    <option value="name">Name</option>
+                                    <option value="email">Email ID</option>
+                                    <option value="address">Address</option>
+                                    <option value="phone">Phone</option>
+                                    <option value="dob">Date Of Birth</option>
+                                    <option value="country">Country</option>
+                                </select>
+                                <input type="text" name="new" class="form-control" placeholder="Enter new value"><br>
+                                <input type="submit" class="form-control btn-success" value="Save">
+                                
+                            </form>
+                        </div>
+                        <!-- php -->
+                        <?php
+                            include 'DataAccess.php';
+                            if($_SERVER['REQUEST_METHOD']=="POST"){
+                                $user = $_SESSION['username'];
+                                $field = $_POST['fields'];
+                                $data = $_POST['new'];
+
+                                try{
+                                    $da = new Database();
+                                    $da->updateUserData($field,$data,$user);
+
+                                }catch(Exception $e){echo "Connection Error:".$e->getMessage();}
+                            }
+                        ?>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <div id="erpheader">
-            <h1><em>Tour Cafe</em></h1>
-    </div>
-    <div id="editForm">
-        <div id="editFields">
-            <h2>Choose:</h2>
-            <form action="editprofile.php" method="post" id="editing">
-                <select name="fields">
-                    <option value="name">Name</option>
-                    <option value="email">Email ID</option>
-                    <option value="address">Address</option>
-                    <option value="phone">Phone</option>
-                    <option value="dob">Date Of Birth</option>
-                    <option value="country">Country</option>
-                </select>
-                <input type="text" name="new" class="inputFields"><br>
-                <input type="submit" value="Edit Profile" class="inputButtons">
-                <?php
-                    include 'DataAccess.php';
-                    if($_SERVER['REQUEST_METHOD']=="POST"){
-                        $user = $_SESSION['username'];
-                        $field = $_POST['fields'];
-                        $data = $_POST['new'];
-
-                        try{
-                            $da = new Database();
-                            $da->updateUserData($field,$data,$user);
-
-                        }catch(Exception $e){echo "Connection Error:".$e->getMessage();}
-                    }
-                ?>
-            </form>
-        </div>
-    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
--- php/userprofile.php
+++ php/userprofile.php
@@ -7,34 +7,80 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
-    <link rel="stylesheet" href="css/user.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/user.css">
 </head>
 <body>
-    <!--navigation bar-->
-    <div class="topNavigation">
-        <div class="topNavButtons" style="float:right">
-                <a href="verify.php">Profile</a>
-                <a href="logout.php">Log Out</a>
+
+    <div class="container-fluid">
+        <!--navigation bar-->
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+        
+            <div class="navbar-header">
+                <a class="navbar-brand" href="../home.htm"><em>Tour Cafe</em></a>
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#myNavBar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavBar">
+                <ul class="nav navbar-nav">
+                    <li><a href="verify.php">Profile</a></li>
+                    <li><a href="logout.php">Log Out</a></li>
+                </ul>
+            </div>
+    
+        </nav>
+        <!-- Body -->
+        <div class="row">
+                <div class="col-lg-12">
+                            
+
+                            <div class="col-xs-12 col-lg-6 boxes">
+                                <div class="bg-info">
+                                            <?php
+                                                include 'DataAccess.php';
+                                                $user = $_SESSION['username'];
+                                                try{
+                                                    $da = new Database();
+                                                    $da->getUserData($user);
+                                                }catch(Exception $e){echo "Error:".$e->getMessage();}
+                                            ?>
+                                </div>
+                                <a class="btn btn-success center-block"  href="editprofile.php">Edit</a>
+                            </div>
+                            <div class="col-xs-12 col-lg-6 boxes">
+                                        <div class="bg-info" id="userpanel">
+                                                <ul class="nav nav-pills">
+                                                    <li class="active"><a data-toggle="pill" href="#tab1"><i class="fa fa-flag"></i></a></li>
+                                                    <li><a data-toggle="pill" href="#tab2"><i class="fa fa-envelope"></i></a></li>
+                                                    <li><a data-toggle="pill" href="#tab3"><i class="fa fa-microphone"></i></a></li>
+                                                </ul>
+                                        
+                                                <div class="tab-content">
+                                                    <div id="tab1" class="tab-pane fade in active">
+                                                    <h3>Archive</h3>
+                                                    <p>No messages on archive.</p>
+                                                    </div>
+                                                    <div id="tab2" class="tab-pane fade">
+                                                    <h3>Inbox</h3>
+                                                    <p>No new messages available.</p>
+                                                    </div>
+                                                    <div id="tab3" class="tab-pane fade">
+                                                    <h3>Announcements</h3>
+                                                    <p>No new announcements available.</p>
+                                                    </div>
+                                                </div>
+                                        </div>
+                            </div>
+
+                    
+                </div>     
         </div>
     </div>
-    <div id="erpheader">
-            <h1><em>Tour Cafe</em></h1>
-    </div>
-    <div id="profileStatus">
-    <?php
-        include 'DataAccess.php';
-        $user = $_SESSION['username'];
-        try{
-            $da = new Database();
-            $da->getUserData($user);
-        }catch(Exception $e){echo "Error:".$e->getMessage();}
-        ?>
-        <div id="notifications">
-            <div id="icons"><i class="fa fa-microphone fa-2x"></i><i class="fa fa-envelope fa-2x"></i><i class="fa fa-flag fa-2x"></i></div>
-            <div id="panel"></div>
-        </div>
-        <a href="editprofile.php">Edit Profile</a>
-    </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
